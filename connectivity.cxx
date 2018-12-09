c9eb9be827f25ef9e925b7b1cd4afb155424accb
zhaoyuw2 <zhaoyuw2@illinois.edu>
2018-12-08 18:22:00 -0600

    add semiring

--- connectivity.cxx
+++ connectivity.cxx
@@ -1,44 +1,40 @@
 #include <ctf.hpp>
 using namespace CTF;
 
-Tensor<int> connectivity(Matrix<int> & A){
-  int n = A.nrow;
-  Vector<int> v(n);
-  Vector<int> w(n);
 
-  //v.get_local_data
-  //change the data
+Tensor<int> connectivity(int n, World & dw){
+  Semiring<int> s(n*n,
+                [](int a, int b){ return std::max(a,b); },
+                MPI_MAX,
+                0,
+                [](int a, int b){ return a+b; });
+  Matrix<int> A(n, n, dw, s);
+  srand48(dw.rank);
+  double sp_frac = 0.1;
+  A.fill_sp_random(1.0,1.0,sp_frac);
+  printf("matrix is: \n");
+  A.print_matrix();
+  Vector<int> W(n, dw, s);
 
-  w["i"] = v["i"];
-
-  for(int i = 1; i <= n; i++){
-    //semering
-    w["j"] += A["jk"]*w["k"];
-
-  }
-  return w;
+  W["i"] += A["ij"] * W["j"];
+  return W;
 }
+
 
 int main(int argc, char ** argv){
   int rank, np;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
-
-  float sp_frac = 0.1;
-  int n = 8;
-  Matrix<int> A(n,n,SH|SP);
-  srand48(A.wrld->rank);
-  A.fill_sp_random(1,1,sp_frac);
+  World dw(argc, argv);
+  Tensor<int> ret = connectivity(7, dw);
   //generate graph with blocks
   /**
   for i = 1 to n/b:
     A[bi:b(i+1), bi:b(i+1)] = B[i]
-    //block sparse
   **/
-  printf("matrix before: \n");
-  A.print_matrix();
+    //block sparse
+
   printf("return w: \n");
-  Vector<int> w = connectivity(A);
-  w.print();
+  ret.print();
 }
