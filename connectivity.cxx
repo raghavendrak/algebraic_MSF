696af5c6bb8fb26956cfb45ca88e6e75df707fcf
yzhng223 <yzhng223@illinois.edu>
2018-12-10 23:05:12 -0600

    Add simple graph class

--- connectivity.cxx
+++ connectivity.cxx
@@ -1,48 +1,131 @@
 #include <ctf.hpp>
+
+#define PLACE_VERTEX (1);
+
 using namespace CTF;
 
+class IntPair {
+public:
+	int64_t i1;
+	int64_t i2;
+	
+	IntPair(int64_t i1, int64_t i2);
+	
+	IntPair(const IntPair &p);
+};
 
-Tensor<int> connectivity(int n, World & dw){
-  Semiring<int> s(0,
-                [](int a, int b){ return std::max(a,b); },
-                MPI_MAX,
-                0,
-                [](int a, int b){ return a+b; });
-  Matrix<int> A(n, n, get_universe(), s);
-  //srand48(dw.rank);
-  //double sp_frac = 0.1;
+class Graph {
+public:
+	int n;
+	vector<IntPair> edges;
+	
+	Graph(int n, vector<IntPair> edges);
+	
+	Matrix<int>* adj_mat(World* w);
+};
 
-  //A.fill_sp_random(1.0,1.0,sp_frac);
-  A["ij"] = 0;
-  int64_t * idx = new int64_t[12]{1, 2, 7, 9, 10, 14, 15, 18, 19, 22, 30, 37};
-  int * data = new int[12]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-  A.write(12, idx, data);
-  printf("matrix is: \n");
-  A.print_matrix();
-  Vector<int> W(n, dw, s);
+Vector<int>* connectivity(Matrix<int>* A);
 
-  for(int j = 0; i < n; j++)
-    W["j"] += A["jk"] * W["k"];
-  free(idx);
-  free(data);
-  return W;
+int main(int argc, char** argv) {
+	int rank;
+	int np;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &np);
+	World w(argc, argv);
+	
+	auto edges = vector<IntPair>();
+	edges.emplace_back(1, 2);
+	edges.emplace_back(3, 4);
+	edges.emplace_back(3, 5);
+	edges.emplace_back(4, 5);
+	auto g = Graph(6, edges);
+	auto B = g.adj_mat(&w);
+	B->print_matrix();
+	connectivity(B)->print();
+	
+	int n = 7;
+	auto A = new Matrix<int>(n, n, w);
+	(*A)["ij"] = 0; // init to all 0
+	int v = 12;
+	auto idx = new int64_t[v]{1, 2, 7, 9, 10, 14, 15, 18, 19, 22, 30, 37};
+	auto data = new int[v];
+	for (int i = 0; i < v; i++) {
+		data[i] = PLACE_VERTEX;
+	}
+	A->write(v, idx, data);
+	printf("matrix before is: \n");
+	A->print_matrix();
+	free(idx);
+	free(data);
+	
+	auto ret = connectivity(A);
+	free(A);
+	printf("return w: \n");
+	ret->print();
+	free(ret);
 }
 
-
-int main(int argc, char ** argv){
-  int rank, np;
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &np);
-  World dw(argc, argv);
-  Tensor<int> ret = connectivity(7, dw);
-  //generate graph with blocks
-  /**
-  for i = 1 to n/b:
-    A[bi:b(i+1), bi:b(i+1)] = B[i]
-  **/
-    //block sparse
-
-  printf("return w: \n");
-  ret.print();
+IntPair::IntPair(int64_t i1, int64_t i2) {
+	this->i1 = i1;
+	this->i2 = i2;
 }
+
+IntPair::IntPair(const IntPair &p) {
+	this->i1 = p.i1;
+	this->i2 = p.i2;
+}
+
+Graph::Graph(int n, vector<IntPair> edges) {
+	this->n = n;
+	this->edges = edges;
+}
+
+Matrix<int>* Graph::adj_mat(World* w) {
+	auto A = new Matrix<int>(n, n, *w);
+	(*A)["ij"] = 0;
+	
+	auto n64 = (int64_t) n;
+	auto m = (int64_t) edges.size();
+	auto m2 = m * 2;
+	auto idx = new int64_t[m2];
+	for (int i = 0; i < m; i++) {
+		auto edge = edges[i];
+		auto v1 = edge.i1;
+		auto v2 = edge.i2;
+		idx[i] = v1 * n64 + v2;
+		idx[i + m] = v2 * n64 + v1;
+	}
+	auto fill = new int[m2];
+	for (int i = 0; i < m2; i++) {
+		fill[i] = PLACE_VERTEX;
+	}
+	A->write(m2, idx, fill);
+	
+	return A;
+}
+
+Vector<int>* connectivity(Matrix<int>* A) {
+	assert(A->nrow == A->ncol);
+	
+	// tropical semiring
+	Semiring<int> tsr(0,
+	                  [](int a, int b) { return std::max(a, b); },
+	                  MPI_MAX,
+	                  0,
+	                  [](int a, int b) { return a + b; });
+	int n = A->nrow;
+	auto w = new Vector<int>(n, *A->wrld, tsr);
+	auto tmp = A->sr;
+	// equip A w/ tsr
+	A->sr = &tsr;
+	
+	// update adj mat
+	for (int j = 0; j < n; j++) {
+		(*w)["j"] += (*A)["jk"] * (*w)["k"];
+	}
+	
+	A->sr = tmp;
+	return w;
+}
+
