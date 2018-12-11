4f97a2609b1957be61c2532c427d02de54721c86
yzhng223 <yzhng223@illinois.edu>
2018-12-11 10:43:26 -0600

    Clean

--- connectivity.cxx
+++ connectivity.cxx
@@ -16,15 +16,28 @@ public:
 
 class Graph {
 public:
+	/**
+	 * number of vertices
+	 */
 	int n;
 	vector<IntPair> edges;
 	
+	/**
+	 * @param n number of vertices
+	 * @param edges list of edges as a pair of two 0-indexed vertices
+	 */
 	Graph(int n, vector<IntPair> edges);
 	
-	Matrix<float>* adj_mat(World* w);
+	Matrix<float>* adj_mat(World* w, bool sparse = false);
 };
 
 Vector<float>* connectivity(Matrix<float> A);
+
+static Semiring<float> TSR(0.0,
+                           [](float a, float b) { return std::max(a, b); },
+                           MPI_MAX,
+                           1.0,
+                           [](float a, float b) { return a * b; });
 
 int main(int argc, char** argv) {
 	int rank;
@@ -34,7 +47,6 @@ int main(int argc, char** argv) {
 	MPI_Comm_size(MPI_COMM_WORLD, &np);
 	World w(argc, argv);
 	
-	cout << "Example 1:" << endl;
 	auto edges = vector<IntPair>();
 	edges.emplace_back(1, 2);
 	edges.emplace_back(3, 4);
@@ -45,31 +57,6 @@ int main(int argc, char** argv) {
 	B->print_matrix();
 	connectivity(*B)->print();
 	free(B);
-	
-	cout << endl;
-	
-	cout << "Example 2:" << endl;
-	int n = 7;
-	Semiring<float> tsr(0.0,
-	                    [](float a, float b) { return std::max(a, b); },
-	                    MPI_MAX,
-	                    1.0,
-	                    [](float a, float b) { return a * b; });
-	auto A = Matrix<float>(n, n, w, tsr);
-	int v = 12;
-	auto idx = new int64_t[v]{1, 2, 7, 9, 10, 14, 15, 18, 19, 22, 30, 37};
-	auto data = new float[v];
-	for (int i = 0; i < v; i++) {
-		data[i] = PLACE_VERTEX;
-	}
-	A.write(v, idx, data);
-	A.print_matrix();
-	free(idx);
-	free(data);
-	
-	auto ret = connectivity(A);
-	ret->print();
-	free(ret);
 }
 
 IntPair::IntPair(int64_t i1, int64_t i2) {
@@ -82,34 +69,28 @@ Graph::Graph(int n, vector<IntPair> edges) {
 	this->edges = std::move(edges);
 }
 
-Matrix<float>* Graph::adj_mat(World* w) {
-	// tropical semiring (tsr)
-	Semiring<float> tsr(0.0,
-	                    [](float a, float b) { return std::max(a, b); },
-	                    MPI_MAX,
-	                    1.0,
-	                    [](float a, float b) { return a * b; });
-	auto A = new Matrix<float>(n, n, *w, tsr);
+Matrix<float>* Graph::adj_mat(World* w, bool sparse) {
+	auto attr = 0;
+	if (sparse) {
+		attr = SP;
+	}
+	auto A = new Matrix<float>(n, n, attr, *w, TSR);
 	
-	auto n64 = (int64_t) n;
 	auto m = (int64_t) edges.size();
 	auto m2 = m * 2;
 	auto idx = new int64_t[m2];
 	for (int i = 0; i < m; i++) {
 		auto edge = edges[i];
-		auto v1 = edge.i1;
-		auto v2 = edge.i2;
-		idx[i] = v1 * n64 + v2;
-		idx[i + m] = v2 * n64 + v1;
+		idx[i] = edge.i1 * n + edge.i2;
+		idx[i + m] = edge.i2 * n + edge.i1;
 	}
-	auto fill = new float[m2];
+	float fill[m2];
 	for (int i = 0; i < m2; i++) {
 		fill[i] = PLACE_VERTEX;
 	}
 	A->write(m2, idx, fill);
 	
 	free(idx);
-	free(fill);
 	return A;
 }
 
@@ -117,12 +98,7 @@ Vector<float>* connectivity(Matrix<float> A) {
 	assert(A.nrow == A.ncol);
 	int n = A.nrow;
 	
-	Semiring<float> tsr(0.0,
-	                    [](float a, float b) { return std::max(a, b); },
-	                    MPI_MAX,
-	                    1.0,
-	                    [](float a, float b) { return a * b; });
-	auto w = new Vector<float>(n, *A.wrld, tsr);
+	auto w = new Vector<float>(n, *A.wrld, TSR);
 	auto idx = new int64_t[n];
 	auto fill = new float[n];
 	for (int i = 0; i < n; i++) {
@@ -130,13 +106,130 @@ Vector<float>* connectivity(Matrix<float> A) {
 		fill[i] = i;
 	}
 	w->write(n, idx, fill);
-	cout << "w setup: " << endl;
-	w->print();
+	free(idx);
+	free(fill);
 	
 	// update adj mat
 	for (int i = 0; i < n; i++) {
 		(*w)["j"] += A["jk"] * (*w)["k"];
-//		w->print();
+	}
+	
+	return w;
+}
+#include <ctf.hpp>
+#include <utility>
+#include <cfloat>
+
+#define PLACE_VERTEX (1.0);
+
+using namespace CTF;
+
+class IntPair {
+public:
+	int64_t i1;
+	int64_t i2;
+	
+	IntPair(int64_t i1, int64_t i2);
+};
+
+class Graph {
+public:
+	/**
+	 * number of vertices
+	 */
+	int n;
+	vector<IntPair> edges;
+	
+	/**
+	 * @param n number of vertices
+	 * @param edges list of edges as a pair of two 0-indexed vertices
+	 */
+	Graph(int n, vector<IntPair> edges);
+	
+	Matrix<float>* adj_mat(World* w, bool sparse = false);
+};
+
+Vector<float>* connectivity(Matrix<float> A);
+
+static Semiring<float> TSR(0.0,
+                           [](float a, float b) { return std::max(a, b); },
+                           MPI_MAX,
+                           1.0,
+                           [](float a, float b) { return a * b; });
+
+int main(int argc, char** argv) {
+	int rank;
+	int np;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &np);
+	World w(argc, argv);
+	
+	auto edges = vector<IntPair>();
+	edges.emplace_back(1, 2);
+	edges.emplace_back(3, 4);
+	edges.emplace_back(3, 5);
+	edges.emplace_back(4, 5);
+	auto g = Graph(6, edges);
+	auto B = g.adj_mat(&w);
+	B->print_matrix();
+	connectivity(*B)->print();
+	free(B);
+}
+
+IntPair::IntPair(int64_t i1, int64_t i2) {
+	this->i1 = i1;
+	this->i2 = i2;
+}
+
+Graph::Graph(int n, vector<IntPair> edges) {
+	this->n = n;
+	this->edges = std::move(edges);
+}
+
+Matrix<float>* Graph::adj_mat(World* w, bool sparse) {
+	auto attr = 0;
+	if (sparse) {
+		attr = SP;
+	}
+	auto A = new Matrix<float>(n, n, attr, *w, TSR);
+	
+	auto m = (int64_t) edges.size();
+	auto m2 = m * 2;
+	auto idx = new int64_t[m2];
+	for (int i = 0; i < m; i++) {
+		auto edge = edges[i];
+		idx[i] = edge.i1 * n + edge.i2;
+		idx[i + m] = edge.i2 * n + edge.i1;
+	}
+	float fill[m2];
+	for (int i = 0; i < m2; i++) {
+		fill[i] = PLACE_VERTEX;
+	}
+	A->write(m2, idx, fill);
+	
+	free(idx);
+	return A;
+}
+
+Vector<float>* connectivity(Matrix<float> A) {
+	assert(A.nrow == A.ncol);
+	int n = A.nrow;
+	
+	auto w = new Vector<float>(n, *A.wrld, TSR);
+	auto idx = new int64_t[n];
+	auto fill = new float[n];
+	for (int i = 0; i < n; i++) {
+		idx[i] = i;
+		fill[i] = i;
+	}
+	w->write(n, idx, fill);
+	free(idx);
+	free(fill);
+	
+	// update adj mat
+	for (int i = 0; i < n; i++) {
+		(*w)["j"] += A["jk"] * (*w)["k"];
 	}
 	
 	return w;
