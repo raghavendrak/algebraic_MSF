54ad215998892a231c7c02afeea430badc83f131
raghavendrak <raghavendra066@gmail.com>
2019-07-08 13:17:17 -0500

    bug fixes, driver changes, code cleanup
    Change-Id: I7a64f2d161cb1a15dc85cb069e17760b184a3c48

--- ctf/hook.cxx
+++ ctf/hook.cxx
@@ -34,40 +34,46 @@ class Graph {
     Matrix<int>* adjacencyMatrix(World* world, bool sparse = false);
 };
 
+void init_pvector(Vector<int>* p)
+{
+  int64_t npairs;
+  Pair<int> * loc_pairs;
+  p->read_local(&npairs, &loc_pairs);
+
+  for (int64_t i = 0; i < npairs; i++){
+    loc_pairs[i].d = loc_pairs[i].k;
+  }
+
+  p->write(npairs, loc_pairs);
+  delete [] loc_pairs;
+}
+
+Matrix<int>* pMatrix(Vector<int> *p, Vector<int> *pg, World* world);
+
+// FIXME: below functions are yet to be optimized/reviewed
+// ---------------------------
 void mat_set(Matrix<int>* matrix, Int64Pair index, int value = PLACE_VERTEX);
-
 int mat_get(Matrix<int>* matrix, Int64Pair index);
-
-void vec_set(Vector<int>* vector, int index, int value = PLACE_VERTEX);
-
-int vec_get(Vector<int>* vector, int index);
-
 Matrix<int>* mat_add(Matrix<int>* A, Matrix<int>* B, World* world);
-
 Matrix<int>* mat_I(int dim, World* world);
-
-Matrix<int>* mat_P(Vector<int>* vector, World* world);
-
 bool mat_eq(Matrix<int>* A, Matrix<int>* B);
-bool vec_eq(Vector<int>* A, Vector<int>* B);
-
 Vector<int>* hook(Graph* graph, World* world);
+void shortcut(Vector<int> & pi);
+// ---------------------------
+
+Vector<int>* supervertex_matrix(int n, Matrix<int> *A, Vector<int> *p, Vector<int> *pg,  World *world);
 Vector<int>* hook_matrix(int n, Matrix<int> * A, World* world);
 
-void vec_max(Vector<int>* out, Vector<int>* in1, Vector<int>* in2);
-void shortcut(Vector<int> & pi);
-
-Vector<int>* supervertex_matrix(int n, Matrix<int> * A, Vector<int>* p, World* world);
-
-  template <typename dtype>
-bool is_different_vector(CTF::Vector<dtype> & A, CTF::Vector<dtype> & B)
+// NOTE: can't use bool as return
+template <typename dtype>
+int64_t are_vectors_different(CTF::Vector<dtype> & A, CTF::Vector<dtype> & B)
 {
-  CTF::Scalar<bool> s;
-  s[""] = CTF::Function<dtype,dtype,bool>([](dtype a, dtype b){ return a!=b; })(A["i"],B["i"]);
+  CTF::Scalar<int64_t> s;
+  s[""] = CTF::Function<dtype,dtype,int64_t>([](dtype a, dtype b){ return a!=b; })(A["i"],B["i"]);
   return s.get_val();
 }
 
-  template <typename dtype>
+template <typename dtype>
 void max_vector(CTF::Vector<dtype> & result, CTF::Vector<dtype> & A, CTF::Vector<dtype> & B)
 {
   result["i"] = CTF::Function<dtype,dtype,dtype>([](dtype a, dtype b){return ((a > b) ? a : b);})(A["i"], B["i"]);
@@ -136,8 +142,12 @@ void test_6Blocks_simply_connected(World *w){
     }
   }
   auto A = g->adjacencyMatrix(w);
-  A->print_matrix();
-  hook(g, w)->print();
+  hook_matrix(36, A, w)->print();
+  int matSize = 36;
+  auto p = new Vector<int>(matSize, *w, MAX_TIMES_SR);
+  init_pvector(p);
+  Vector<int> *pg = new Vector<int>(*p);
+  supervertex_matrix(matSize, A, p, pg, w)->print();
 }
 
 void test_6Blocks_fully_connected(World *w){
@@ -183,38 +193,44 @@ Matrix<int>* generate_kronecker(World* w, int order){
     // hook on B
   }
   delete kinitiator;
-  assert(matSize == 1594323);
   return B;
 }
 
 void driver(World *w) {
-  /*
-  Matrix<int> *B = new Matrix<int>(6,6,SP|SH,*w,MAX_TIMES_SR);
-  B->fill_sp_random(1.0,1.0,0.1);
-  printf("6X6 Matrix <1.0, 1.0, 0.1>\n");
-  B->print_matrix();
-  */
-  // hook_matrix(6, B, w)->print();
-
   // K13 : 1594323 (matrix size)
-  int64_t matSize = 1594323;
-  auto B = generate_kronecker(w, 13);
- 
+  // K6 : 729; 531441 vertices
+  // k5 : 243
+  // k7 : 2187
+  // k8 : 6561
+  // k9 : 19683
+  int64_t matSize = 243;
+  auto B = generate_kronecker(w, 5);
+
   Timer_epoch thm("hook_matrix");
   thm.begin();
-  hook_matrix(matSize, B, w);
+  auto hm = hook_matrix(matSize, B, w);
   thm.end();
 
   auto p = new Vector<int>(matSize, *w, MAX_TIMES_SR);
-  for (int64_t i = 0; i < matSize; i++) {
-    vec_set(p, i, i);
-  }
-  Timer_epoch tsv("super vertex");
+  init_pvector(p);
+  Timer_epoch tsv("super_vertex");
   tsv.begin();
-  supervertex_matrix(matSize, B, p, w);
+  auto pg = new Vector<int>(matSize, *w, MAX_TIMES_SR);
+  init_pvector(pg);
+  auto sv = supervertex_matrix(matSize, B, p, pg, w);
   tsv.end();
 
+  int64_t result = are_vectors_different(*hm, *sv);
+  if (w->rank == 0) {
+    if (result) {
+      printf("result vectors are different: FAIL\n");
+    }
+    else {
+      printf("result vectors are same: PASS\n");
+    }
+  }
   delete B;
+  test_6Blocks_simply_connected(w);
 }
 
 int main(int argc, char** argv) {
@@ -224,64 +240,160 @@ int main(int argc, char** argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
   auto w = new World(argc, argv);
-  /*
-  test_simple(w);
-  test_disconnected(w);
-  test_fully_connected(w);
-  test_random1(w);
-  test_random2(w);
-  test_6Blocks_simply_connected(w);
-     
-  test_6Blocks_fully_connected(w);
-  */
-  // test_simple_kronecker(w);
   driver(w);
   return 0;
 }
 
-Vector<int>* hook(Graph* graph, World* world) {
-  auto n = graph->numVertices;
-  auto A = graph->adjacencyMatrix(world);
-  return hook_matrix(n, A, world);
-}
 
 Vector<int>* hook_matrix(int n, Matrix<int> * A, World* world) {
   auto p = new Vector<int>(n, *world, MAX_TIMES_SR);
-  for (auto i = 0; i < n; i++) {
-    vec_set(p, i, i);
-  }	
+  init_pvector(p);
+  Vector<int> *pg = new Vector<int>(*p);
   auto prev = new Vector<int>(n, *world, MAX_TIMES_SR);
 
-  // ========== ==========
-  while (is_different_vector(*p, *prev)) {
-    // ========== ==========
+  while (are_vectors_different(*p, *prev)) {
     (*prev)["i"] = (*p)["i"];
-    // NOTE: bug here; prev = p;
     auto q = new Vector<int>(n, *world, MAX_TIMES_SR);
     (*q)["i"] = (*A)["ij"] * (*p)["j"];
     auto r = new Vector<int>(n, *world, MAX_TIMES_SR);
     max_vector(*r, *p, *q);
-    auto P = mat_P(p, world);
+    auto P = pMatrix(p, pg, world);
     auto s = new Vector<int>(n, *world, MAX_TIMES_SR);
     (*s)["i"] = (*P)["ji"] * (*r)["i"];
     max_vector(*p, *p, *s);
     Vector<int> * pi = new Vector<int>(*p);
-    //shortcut(*p);
+    shortcut(*p);
 
-    while (is_different_vector(*pi, *p)){
+    while (are_vectors_different(*pi, *p)){
       free(pi);
       pi = new Vector<int>(*p);
       shortcut(*p);
     }
+    free(pi);
 
     free(q);
     free(r);
     free(P);
     free(s);		
   }
+  free(pg);
   return p;
 }
 
+Matrix<int>* pMatrix(Vector<int>* p, Vector<int> *pg, World* world) {
+  /*
+  auto n = p->len;
+  auto A = new Matrix<int>(n, n, SP, *world, MAX_TIMES_SR);
+
+  (*A)["ij"] = CTF::Function<int,int,int>([](int a, int b){ return a==b; })((*p)["i"],(*pg)["j"]);
+  return A;
+  */
+  // FIXME: below is not benchmarked, nor sure if this is the right way of doing it
+  auto n = p->len;
+  auto A = new Matrix<int>(n, n, SP, *world, MAX_TIMES_SR);
+  int64_t npairs;
+  Pair<int> * loc_pairs;
+  p->read_local(&npairs, &loc_pairs);
+  int64_t *gIndex = new int64_t[npairs];
+  int *gData = new int[npairs];
+  for (int64_t i = 0; i < npairs; i++){
+    gIndex[i] = loc_pairs[i].k + loc_pairs[i].d * n;
+    gData[i] = 1;
+  }
+  A->write(npairs, gIndex, gData);
+  delete [] gIndex;
+  delete [] gData;
+  return A;
+}
+
+void shortcut(Vector<int> & pi){
+  int64_t npairs;
+  Pair<int> * loc_pairs;
+  // obtain all values of pi on this process
+  pi.read_local(&npairs, &loc_pairs);
+  Pair<int> * remote_pairs = new Pair<int>[npairs];
+  // set keys to value of pi, so remote_pairs[i].k = pi[loc_pairs[i].k]
+  for (int64_t i=0; i<npairs; i++){
+    //cout << "k: " << remote_pairs[i].k << " d: " << loc_pairs[i].d << endl;
+    remote_pairs[i].k = loc_pairs[i].d;
+  }
+  // obtains values at each pi[i] by remote read, so remote_pairs[i].d = pi[loc_pairs[i].k]
+  pi.read(npairs, remote_pairs);
+  // set loc_pairs[i].d = remote_pairs[d] and write back to local data
+  for (int64_t i=0; i<npairs; i++){
+    loc_pairs[i].d = remote_pairs[i].d;
+  }
+  delete [] remote_pairs;
+  pi.write(npairs, loc_pairs);
+  delete [] loc_pairs;
+}
+
+Vector<int>* supervertex_matrix(int n, Matrix<int>* A, Vector<int>* p, Vector<int> *pg, World* world) {
+  auto q = new Vector<int>(n, *world, MAX_TIMES_SR);
+  (*q)["i"] = (*p)["i"] + (*A)["ij"] * (*p)["j"];
+  if (!are_vectors_different(*p, *q)) {
+    return q;
+  }
+  else {
+    auto P = pMatrix(q, pg, world);
+    auto rec_A = new Matrix<int>(n, n, SP, *world, MAX_TIMES_SR);
+    (*rec_A)["il"] = (*P)["ji"] * (*A)["jk"] * (*P)["kl"];
+    auto rec_p = supervertex_matrix(n, rec_A, q, pg, world);
+    delete rec_A;
+    // p[i] = rec_p[q[i]]
+    int64_t npairs;
+    Pair<int>* loc_pairs;
+    q->read_local(&npairs, &loc_pairs);
+    Pair<int> * remote_pairs = new Pair<int>[npairs];
+    for (int64_t i = 0; i < npairs; i++) {
+      remote_pairs[i].k = loc_pairs[i].d;
+    }
+    rec_p->read(npairs, remote_pairs);
+    for (int64_t i = 0; i < npairs; i++) {
+      loc_pairs[i].d = remote_pairs[i].d;
+    }
+    // FIXME: assumption: p & q use the same distribution across processes
+    p->write(npairs, loc_pairs);
+    delete [] remote_pairs;
+    delete [] loc_pairs;
+    return p;
+  }
+  free(pg);
+}
+
+// FIXME: below functions are yet to be optimized/reviewed
+// ---------------------------
+Vector<int>* hook(Graph* graph, World* world) {
+  auto n = graph->numVertices;
+  auto A = graph->adjacencyMatrix(world);
+  return hook_matrix(n, A, world);
+}
+
+Matrix<int>* mat_add(Matrix<int>* A, Matrix<int>* B, World* world) {
+  int n = A->nrow;
+  int m = A->ncol;
+  auto C = new Matrix<int>(n, m, *world, MAX_TIMES_SR);
+  for (auto row = 0; row < n; row++) {
+    for (auto col = 0; col < m; col++) {
+      auto idx = Int64Pair(row, col);
+      auto aVal = mat_get(A, idx);
+      auto bVal = mat_get(B, idx);
+      mat_set(C, idx, min(1, aVal + bVal));
+    }
+  }
+  return C;
+}
+
+bool mat_eq(Matrix<int>* A, Matrix<int>* B) {
+  for (int r = 0; r < A->nrow; r++) {
+    for (int c = 0; c < A->ncol; c++) {
+      if (mat_get(A, Int64Pair(r, c)) != mat_get(B, Int64Pair(r, c))) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
 Int64Pair::Int64Pair(int64_t i1, int64_t i2) {
   this->i1 = i1;
@@ -334,151 +446,4 @@ int mat_get(Matrix<int>* matrix, Int64Pair index) {
   free(data);
   return value;
 }
-
-void vec_set(Vector<int>* vector, int index, int value) {
-  int64_t idx[1];
-  idx[0] = index;
-  int fill[1];
-  fill[0] = value;
-  vector->write(1, idx, fill);
-}
-
-int vec_get(Vector<int>* vector, int index) {
-  auto data = new int[vector->len];
-  vector->read_all(data);
-  int value = data[index];
-  free(data);
-  return value;
-}
-
-Matrix<int>* mat_P(Vector<int>* vector, World* world) {
-  auto n = vector->len;
-  auto A = new Matrix<int>(n, n, *world, MAX_TIMES_SR);
-  for (auto row = 0; row < n; row++) {
-    for (auto col = 0; col < n; col++) {
-      if (vec_get(vector, row) == col) {
-        mat_set(A, Int64Pair(row, col), 1);
-      } else {
-        mat_set(A, Int64Pair(row, col), 0);
-      }
-    }
-  }
-  return A;
-}
-
-Matrix<int>* mat_add(Matrix<int>* A, Matrix<int>* B, World* world) {
-  int n = A->nrow;
-  int m = A->ncol;
-  auto C = new Matrix<int>(n, m, *world, MAX_TIMES_SR);
-  for (auto row = 0; row < n; row++) {
-    for (auto col = 0; col < m; col++) {
-      auto idx = Int64Pair(row, col);
-      auto aVal = mat_get(A, idx);
-      auto bVal = mat_get(B, idx);
-      mat_set(C, idx, min(1, aVal + bVal));
-    }
-  }
-  return C;
-}
-
-bool mat_eq(Matrix<int>* A, Matrix<int>* B) {
-  for (int r = 0; r < A->nrow; r++) {
-    for (int c = 0; c < A->ncol; c++) {
-      if (mat_get(A, Int64Pair(r, c)) != mat_get(B, Int64Pair(r, c))) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
-bool vec_eq(Vector<int>* A, Vector<int>* B) {
-  if (A->len != B->len) {
-    return false;
-  }
-  for (int i = 0; i < A->len; i++){
-    if(vec_get(A, i) != vec_get(B, i)){
-      return false;
-    }
-  }
-  return true;
-}
-
-void vec_max(Vector<int>* out, Vector<int>* in1, Vector<int>* in2) {
-  for(int i = 0; i < out->len; i++) {
-    int pi = vec_get(in1, i);
-    int qi = vec_get(in2, i);
-    if (pi > qi) {
-      vec_set(out, i, pi);
-    }
-    else {
-      vec_set(out, i, qi);
-    }
-  }
-}
-
-void shortcut(Vector<int> & pi){
-  int64_t npairs;
-  Pair<int> * loc_pairs;
-  // obtain all values of pi on this process
-  pi.read_local(&npairs, &loc_pairs);
-  Pair<int> * remote_pairs = new Pair<int>[npairs];
-  // set keys to value of pi, so remote_pairs[i].k = pi[loc_pairs[i].k]
-  for (int64_t i=0; i<npairs; i++){
-    //cout << "k: " << remote_pairs[i].k << " d: " << loc_pairs[i].d << endl;
-    remote_pairs[i].k = loc_pairs[i].d;
-  }
-
-
-  // obtains values at each pi[i] by remote read, so remote_pairs[i].d = pi[loc_pairs[i].k]
-  pi.read(npairs, remote_pairs);
-  // set loc_pairs[i].d = remote_pairs[d] and write back to local data
-  for (int64_t i=0; i<npairs; i++){
-    loc_pairs[i].d = remote_pairs[i].d;
-  }
-  delete [] remote_pairs;
-  pi.write(npairs, loc_pairs);
-  delete [] loc_pairs;
-}
-
-Vector<int>* supervertex_matrix(int n, Matrix<int>* A, Vector<int>* p, World* world) {
-  auto q = new Vector<int>(n, *world, MAX_TIMES_SR);
-  (*q)["i"] = (*p)["i"] + (*A)["ij"] * (*p)["j"];
-  if (!is_different_vector(*p, *q)) {
-    return q;
-  }
-  else {
-    auto P = mat_P(q, world);
-    auto rec_A = new Matrix<int>(n, n, SP, *world, MAX_TIMES_SR);
-    (*rec_A)["il"] = (*P)["ji"] * (*A)["jk"] * (*P)["kl"];
-    auto rec_p = supervertex_matrix(n, rec_A, q, world);
-    delete rec_A;
-
-    // p[i] = rec_p[q[i]]
-    int64_t npairs;
-    Pair<int>* loc_pairs;
-    q->read_local(&npairs, &loc_pairs);
-    Pair<int> * remote_pairs = new Pair<int>[npairs];
-    for (int64_t i = 0; i < npairs; i++) {
-      remote_pairs[i].k = loc_pairs[i].d;
-    }
-    rec_p->read(npairs, remote_pairs);
-    for (int64_t i = 0; i < npairs; i++) {
-      loc_pairs[i].d = remote_pairs[i].d;
-    }
-    // FIXME: assumption: p & q use the same distribution across processes
-    p->write(npairs, loc_pairs);
-    delete [] remote_pairs;
-    delete [] loc_pairs;
-    return p;
-  }
-}
-
-/**
-  template typename <dtype>
-  bool is_different(CTF::Matrix<dtype> A, CTF::Matrix<dtype> B){
-  CTF::Scalar<bool> s(); // maybe need to define semiring, but maybe default works
-  s[""] = CTF::Function<dtype,dtype,bool>([](dtype a, dtype b){ return a!=b; })(A["ij"],B["ij"]);
-  return s.get_val(); // not sure this is the right function, but there should be one to extract the value of a scalar
-  }
- **/
+// ---------------------------
